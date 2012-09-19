f585d3cac552026da1fcf7c8c3303c699b38fa49
adrobnych <adrobnych@gmail.com>
2012-09-19 17:47:39 +0300

    added model view controller for girl names

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	girls = ["Tanya", "Sveta", "Marta", "Erika"]
-  	render text: "Hello from #{girls[rand(4)]}"
+  	@name = Girl.names[rand(4)]
   end
 end
